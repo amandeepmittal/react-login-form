1dd550681e00d447ae309a1f8eaf8fee70721374
Aman Mittal <amandeepmittal@live.com>
2022-06-13 18:53:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useTranslation } from 'react-i18next';
+
 import './App.css';
 
+const langs = {
+  en: { nativeName: 'English' },
+  es: { nativeName: 'Español' },
+};
+
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('Your form is submitted!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="form">
+        <h1>{t('formTitle')}</h1>
+        <p className="subtitle">{t('description')}</p>
+        <div>
+          Choose a language to continue:{' '}
+          {Object.keys(langs).map(lang => (
+            <button
+              key={lang}
+              style={{
+                fontWeight: i18n.resolvedLanguage === lang ? 'bold' : 'normal',
+              }}
+              type="submit"
+              onClick={() => i18n.changeLanguage(lang)}
+            >
+              {langs[lang].nativeName}
+            </button>
+          ))}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <label>Email</label>
+            <input type="text" name="email" required />
+          </div>
+          <div className="input-container">
+            <label>Password</label>
+            <input type="password" name="password" required />
+          </div>
+          <button className="button-container" type="submit">
+            <p className="button-text">{t('buttonText')}</p>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
